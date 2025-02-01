b5f2fc980e90ef2d7b62a95bd8b23da1c04f32c7
name1980 <petrovsd2002@gmail.com>
2025-02-01 17:40:30 +0300

    Удален ненужный PagingSource

--- data/src/main/kotlin/ru/coursefinder/data/model/Meta.kt
+++ data/src/main/kotlin/ru/coursefinder/data/model/Meta.kt
@@ -8,14 +8,4 @@ internal data class Meta(
     val page: Int,
     @SerialName("has_next") val hasNext: Boolean,
     @SerialName("has_previous") val hasPrevious: Boolean
-) {
-    val previousPage: Int? get() = when {
-        hasPrevious -> page - 1
-        else -> null
-    }
-
-    val nextPage: Int? get() = when {
-        hasNext -> page + 1
-        else -> null
-    }
-}
+)
